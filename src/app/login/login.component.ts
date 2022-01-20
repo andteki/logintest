2ba4525505c6a7c625d93b92e9acadd0f7f2b4f3
Sallai András <andrewteki@gmail.com>
2022-01-20 12:48:43 +0100

    'Kezdés'

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from '../shared/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm !: FormGroup
+
+  constructor(
+    private auth: AuthService,
+    private formBuilder: FormBuilder
+    ) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      user: [''],
+      pass: ['']
+    });
+  }
+
+  login() {
+    let user = this.loginForm.value.user;
+    let pass = this.loginForm.value.pass;
+    console.log(user)
+    console.log(pass)
+
+    this.auth.login(user, pass)
+    .subscribe(res => {
+      console.log(res);
+      if (res.success) {
+        localStorage.setItem('currentUser', 
+        JSON.stringify({token: res.data.token, name: res.data.name})
+        );
+        
+
+      }
+    })
+
+  }
+
+}
---
+++ src/app/shared/auth.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  host = 'http://localhost:8000/api/';
+  constructor(private http: HttpClient) { }
+
+  login(user: string, pass: string) {
+    console.log(user)
+    console.log(pass)
+    
+    let authData = {
+      name: user,
+      password: pass
+    }
+    let data = JSON.stringify(authData);
+
+    let headerObj = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    let header = {
+      headers: headerObj
+    }
+
+    let endpoint = 'login';
+    let url = this.host + endpoint;
+    return this.http.post<any>(url, data, header);
+  }
+}
